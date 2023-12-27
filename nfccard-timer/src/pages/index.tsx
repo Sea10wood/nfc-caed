55b37f0ba8b98a426c558a4fa7e1db0c517192de
Sea10wood <miyusea5@gmail.com>
2023-12-28 00:35:12 +0900

    Hellowoldの削除

--- nfccard-timer/src/pages/index.tsx
+++ nfccard-timer/src/pages/index.tsx
@@ -7,8 +7,7 @@ import Image from "next/image";
 export default function Home(this: any) {
   return (
     <div className="bg-gradient-to-b from-cyan-500 via-sky-600 to-blue-900 min-w-screen min-h-screen">
-      <h1 className="text-xl font-bold text-yellow-500">Hello World</h1>
-      <h1 className="text-xl font-bold text-lime-300">Hello World</h1>
+      
       <CountdownTimer />
       <TodoItem />
       <TodoItem />
