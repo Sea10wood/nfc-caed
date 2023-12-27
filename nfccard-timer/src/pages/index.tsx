b9eaa79127b88f8ae8ed2bda03a4ed7729605fb7
Sea10wood <miyusea5@gmail.com>
2023-12-27 19:05:45 +0900

    カウントダウンタイマーの表示

--- nfccard-timer/src/pages/index.tsx
+++ nfccard-timer/src/pages/index.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import CountdownTimer from "@/components/Countdown";
+import React from "react";
+
+export default function Home(this: any) {
   return (
-    <div>
-      <h1 className="text-xl font-bold text-blue-400">Hello World</h1>
+    <div className="bg-gradient-to-b from-cyan-500 via-sky-600 to-blue-900 h-320">
+      <h1 className="text-xl font-bold text-yellow-500">Hello World</h1>
+      <h1 className="text-xl font-bold text-lime-300">Hello World</h1>
+      <CountdownTimer />
     </div>
   );
 }
